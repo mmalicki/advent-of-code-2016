be02130f0d20ae8a9eed5f27a1bbd4e35d1eaf4e
Michal Malicki <michal.malicki@nokia.com>
2017-01-26 09:48:24 +0100

    Refactored room sorting

--- src/main/java/day4/validator/RoomValidator.java
+++ src/main/java/day4/validator/RoomValidator.java
@@ -2,25 +2,23 @@ package day4.validator;
 
 import day4.converter.RoomToRoomNameLetterOccurencesConverter;
 import day4.model.Room;
-
-import java.util.*;
+import java.util.Comparator;
+import java.util.Map;
+import static java.util.Map.*;
+import java.util.TreeMap;
 
 public class RoomValidator {
     private static final int CHECKED_LETTERS_LIMIT = 5;
-    private static final Comparator<Map.Entry<String, Integer>> COMPARE_BY_OCCURRENCES_AND_ALPHABETIZATION = (o1, o2) -> {
-        return o1.getValue().equals(o2.getValue()) ?
-                o1.getKey().compareTo(o2.getKey()) :
-                o2.getValue().compareTo(o1.getValue()); // TODO: CZYTELNOŚĆ !!!!
-    };
 
     public static boolean isValid(Room room) {
         Map<String, Integer> nameLettersOccurrences = new TreeMap<>(RoomToRoomNameLetterOccurencesConverter.convert(room));
-        String validChecksum = nameLettersOccurrences.entrySet().stream()
-        .sorted(COMPARE_BY_OCCURRENCES_AND_ALPHABETIZATION)
-        .map(Map.Entry::getKey)
+        return nameLettersOccurrences.entrySet().stream()
+        .sorted(Comparator
+                .<Entry<String, Integer>, Integer>comparing(Entry::getValue).reversed()
+                .thenComparing(Entry::getKey))
+        .map(Entry::getKey)
         .limit(CHECKED_LETTERS_LIMIT)
-        .reduce(String::concat).orElse(""); // TODO: nieciekawe to
-
-        return room.getChecksum().equals(validChecksum);
+        .reduce(String::concat).orElseGet(String::new)
+        .equals(room.getChecksum());
     }
 }
